0255999400b99cbffd78a1311020c8f277344856
Chris <chrismazza@proton.me>
2023-05-17 04:46:48 -0500

    change request method to POST

--- app/updateserver/views.py
+++ app/updateserver/views.py
@@ -5,7 +5,7 @@ updateserver = Blueprint("updateserver", __name__)
 @updateserver.route('/update_server', methods=['POST','GET'])
 def webhook():
     print('in webhook')
-    if request.method == 'GET':
+    if request.method == 'POST':
         print(f'Before repo assignment')
         repo = Repo('/home/chrism/flask_application_prototype')
         #repo = Repo('/home/chris/bailey-tech/source-code/flask_application_prototype')
