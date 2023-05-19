25c0ea165a65ce2be22e8c25205566a5933105a4
Chris <chrismazza@proton.me>
2023-05-19 12:37:37 -0500

    updated code from https://stackoverflow.com/questions/48047123/syncing-github-with-pythonanywhere

--- app/updateserver/views.py
+++ app/updateserver/views.py
@@ -4,15 +4,14 @@ updateserver = Blueprint("updateserver", __name__)
 
 @updateserver.route('/update_server', methods=['POST'])
 def webhook():
-    print('in webhook fx')
     if request.method == 'POST':
-        print(f'Before repo assignment')
         repo = Repo('/home/chrism/flask_application_prototype')
-        #repo = Repo('/home/chris/bailey-tech/source-code/flask_application_prototype')
-        print(f'repo {repo}')
+        # repo = Repo('/home/chris/bailey-tech/source-code/flask_application_prototype')
         origin = repo.remotes.origin
-        print(f'origin {origin}')
-        origin.pull('https://github.com/chris-mastic/flask_application_prototype', 'master')
-        return 'Updated PythonAnywhere successfully', 200
+        repo.create_head('master',
+                         origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        origin.pull()
+        return '', 200
     else:
-        return 'Wrong event type', 400
+        return '', 400
+
