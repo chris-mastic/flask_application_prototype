6b78f3721d237f600f627ff5f385c4eb09930aec
Chris <chrismazza@proton.me>
2023-05-19 12:48:52 -0500

    Revert webhook from https://stackoverflow.com/questions/48047123/syncing-github-with-pythonanywhere

--- app/updateserver/views.py
+++ app/updateserver/views.py
@@ -2,16 +2,17 @@ from flask import request, Blueprint
 from git import Repo
 updateserver = Blueprint("updateserver", __name__)
 
-@updateserver.route('/update_server', methods=['POST','GET'])
+@updateserver.route('/update_server', methods=['POST'])
 def webhook():
+    print('in webhook fx')
     if request.method == 'POST':
+        print(f'Before repo assignment')
         repo = Repo('/home/chrism/flask_application_prototype')
-        # repo = Repo('/home/chris/bailey-tech/source-code/flask_application_prototype')
+        #repo = Repo('/home/chris/bailey-tech/source-code/flask_application_prototype')
+        print(f'repo {repo}')
         origin = repo.remotes.origin
-        repo.create_head('master',
-                         origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-        origin.pull()
-        return '', 200
+        print(f'origin {origin}')
+        origin.pull('https://github.com/chris-mastic/flask_application_prototype', 'master')
+        return 'Updated PythonAnywhere successfully', 200
     else:
-        return '', 400
-
+        return 'Wrong event type', 400
